a05206dc1e4b3bd460b3eaab949955bbf624c6f6
manhame <respectbiz@gmail.com>
2024-01-09 13:50:27 +0100

    exo14 fin

--- exo14bis.php
+++ exo14bis.php
@@ -8,36 +8,48 @@ class Voiture {
   private string $_marque;
   private string $_modele;
 
-  public function __construct ($marque,$modele){
-    $this -> _marque = $marque;
-    $this -> _modele = $modele;
-  }
+    public function __construct (string $marque, string $modele){
+        $this -> _marque = $marque;
+        $this -> _modele = $modele;
+    }
 
-
-    public function getMarque () {
+    public function getMarque() {
         return $this -> _marque;
     }
-    public function getModele () {
+    public function getModele() {
         return $this -> _modele;
     }
+
     public function setMarque (string $marque) {
         $this -> _marque = $marque;
     }
+    
     public function setModele (string $modele) {
         $this -> _modele = $modele;
     }
 }
 
 class VoitureElec extends Voiture {
-    private int $_autonomie;
+    private $_autonomie;
 
-    public function __construct ($data,$autonomie) {
-        parent ::__construct ($data);
+    public function __construct (string $marque, string $modele, int $autonomie) {
+        parent ::__construct ($marque, $modele);
         $this -> _autonomie = $autonomie;
     }
-}
-$v1 = new Voiture (array("marque"=>"Peugeot","modele"=>"408"));
-$ve1 = new VoitureElec (array("marque" =>"BMW","modele" => "i3"),150);
+              
+    public function getAutonomie(){
+        return $this -> _autonomie;
+    }
 
-echo $v1 -> getData ($data);
-echo $ve1 - getData ($data,"autonomie");
+    public function setAutonomie(int $autonomie) {
+        $this -> _autonomie= $autonomie;        
+    }
+}
+$v1 = new Voiture ("Peugeot","408");
+$ve1 = new VoitureElec ("BMW","i3",150);
+
+echo $v1 -> getMarque()." ";
+echo $v1 -> getModele()."<br>";
+echo $ve1 -> getMarque()." ";
+echo $ve1 -> getModele()." ";
+echo $ve1 -> getAutonomie();
